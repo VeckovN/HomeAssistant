822ec00e1c6096e48efe737c5750b652d8f9ad69
VeckovN <novaknolewow@gmail.com>
2022-12-19 16:25:12 +0100

    Add Client model and routes

--- api/routes/clients.js
+++ api/routes/clients.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
-const {getClientByUsername, getClients, getComments} = require('../controller/clientController');
+const {getClientByUsername, getClients, getComments, rateHouseworker} = require('../controller/clientController');
 
 const router = express.Router();
 
 router.get('/', getClients);
 router.get('/:username', getClientByUsername);
 router.get('/comments/:username', getComments)
-
+router.post('/rate', rateHouseworker);
+// router.post('/update', updateClient);
 
 module.exports = router;
