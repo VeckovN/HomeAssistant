005906c0d6b3c2b1db7e8ff93d7f7d7322fa3352
VeckovN <novaknolewow@gmail.com>
2022-12-19 16:24:12 +0100

    neo4j+s replaced with bolt+s URI

--- api/db/neo4j.js
+++ api/db/neo4j.js
@@ -1,17 +1,20 @@
 const neo4j = require('neo4j-driver');
+const dotenv = require('dotenv')
+dotenv.config();
 
 //ne4j configuration will be stored in .env
-const uri='neo4j+s://fd79543d.databases.neo4j.io';
-const neo4j_user = 'neo4j';
-const neo4j_password = '9YoFUA3ZpZbzhsmSdIOTuxc3oXGRuHOuLhwlNQKyMX4';
+// const uri='neo4j+s://fd79543d.databases.neo4j.io';
+// const neo4j_user = 'neo4j';
+// const neo4j_password = 'HomeAssistantNeo4j';
 
-const driver = neo4j.driver(uri, neo4j.auth.basic(neo4j_user, neo4j_password));
+const driver = neo4j.driver(process.env.NEO4J_URI, neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD));
 const session = driver.session();
 
 
 module.exports = session;
 
 
+//PywSSJA3hJEFm8mnRB7PfndDl1bd9F9IV63gmwpsKto
 
 //Difference betweeen the Session.run() and transaction.run() 
 //https://stackoverflow.com/questions/39525713/session-run-vs-transaction-run-in-neo4j-bolt
