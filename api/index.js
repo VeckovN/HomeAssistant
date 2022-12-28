efbf4c18b8db67e3a7f182ef981500ff5a9ea74d
VeckovN <novaknolewow@gmail.com>
2022-12-28 20:11:39 +0100

    New Chat functions

--- api/index.js
+++ api/index.js
@@ -6,12 +6,14 @@ const session = require('express-session');
 const clientRoute = require('./routes/clients')
 const houseworkerRoute = require('./routes/houseworkers');
 const authRoute = require('./routes/auth');
+const chatRoute = require('./routes/chat');
 const dotenv = require('dotenv');
-const {client:redisClient, RedisStore, set } = require('./db/redis');
+const {client:redisClient, RedisStore, set, sadd, smembers, hmget, srem } = require('./db/redis');
 dotenv.config();
 
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 
 //redis
@@ -50,18 +52,15 @@ app.use(session({
     }
 }))
 
-// app.use(express.urlencoded({ extended: false }))
-// parse application/json
-// app.use(express.json())
 
-//don't needed for newest express-session version
-// app.use(cookieParser());
 
 
 //routes
 app.use("/api/clients", clientRoute);
 app.use("/api/homeworker", houseworkerRoute);
 app.use("/api/" , authRoute);
+app.use('/api/chat', chatRoute);
+
 app.get("/api/", (req,res)=>{
     console.log("PS");
     console.log("SESSSSSLogion3333333333333: " + JSON.stringify(req.session))
@@ -84,10 +83,13 @@ app.use(cors(corsOptions));
 
 //SocketIO 
 const http = require('http');
-const socketio = require('socket.io');
+// const socketio = require('socket.io');
+const {Server} = require('socket.io');
 const server = http.createServer(app);
-const io = socketio(server).listen(server);
-//const io = require("socket.io")(server);
+// const io = socketio(server).listen(server);
+const io = new Server(server);
+
+//when client connected (on client side) this will be  triggered
 
 server.listen(5000, ()=>{
     console.log("SERVER at 5000 port")
@@ -104,6 +106,17 @@ const port ='5000';
 const serverID = `${ip}:${port}`
 console.log("IP:" + ip);
 
+//public and subscribe initialization
+redisClient.on("message", (_,message)=>{
+    const {idServer, type, data} = JSON.parse(message);
+    //serverID is unique for every server (serverIPAddress+port)
+    if(serverID === idServer)
+        return;
+    io.emit(type, data);
+})
+//every client is subscriber
+redisClient.subscribe('MESSAGES');
+
 //public data on "MESSAGE" channel
 const publish = (type, data) =>{
     const dataSent ={
@@ -111,31 +124,138 @@ const publish = (type, data) =>{
         type,
         data
     };
-    redisClient.publish("MESSAGES", JSON.stringify(dataSent));
+redisClient.publish("MESSAGES", JSON.stringify(dataSent));
 }
 
-//every client is subscribed to this channel ("MESSAGE")
-//to receive notification when is message received (to roomID)
-//example when sent message
-//publish("Message",messageOBJ);
-//io.to(roomKey).emit("message", message); //message sent to roomKey=>room:${roomID} 
-//and emit to everyone which listen 'message' channel
-const initPubSub = () =>{
-    //if 
-    redisClient.on('message', (_, message)=>{
-        //dont handle pub/sub message if the server is the same
-        const {serverid , type, data } = JSON.parse(message);
-        if(serverID === serverid)
-            return
-        io.emit(type, data); //this emit 'data' to every member of changel 'type'
-    });
+//we have this socket connection (front) -(back)
+// socket.off("user.connected");
+// socket.off("user.disconnected");
+// socket.off("user.room");
+// socket.off("message");
 
-}
 
-//when socket 
-io.on('connection', (socket)=>{
-    console.log("SOCKET: " + socket.id)
+//FIRST WAIT ON CLIENT EVENT
+//when socket connected - when user (on client) connected to app
+io.on('connection', async(socket)=>{
+    console.log('user connected');
+
+    //socket session is connected as redis session
+    const userInfo = socket.request.session.user;
+    const username = userInfo.username;
+    //user:{id}
+    const userIDKey = await get(`username:${username}`)
+    const userID = userIDKey.split(':')[1]; //{id}
+
+    //add him in online users
+    await sadd('online_users', userID);
+
+
+    //use redis publish to notify all subscribers that user is connected
+    //publish function or client.publish
+    redisClient.publish('user.connected', 'msg');
+    //use socket() to broadcast emit(real-time) TO NOTIFY FRONTEND
+    socket.broadcast.emit('user.contected', 'msg');
+
+    //if client trigger event room.join then
+    socket.on("room.join", id =>{ //listen on 'room.join' event
+        socket.join(`room:${id}`); //join room -> room:1:2 example or group room:1:5:7
+    })
+
+
+    //WHERE CLIENT SEND MESSAGE(ON FRONT SIDE) TAKE THIS MESSAGE AND USE IT IN BACK HERE
+    //listten on message event(if client send message)
+    //we take message from client(message) and persist it to REDIS 
+    //then notify other subscirber of THAT CHANNEL(room) to see sent message
+    //in client --- (socekt.emit('message', {messageObj}))
+    //that trigger this event
+    socket.on("message", async(messageObj)=>{    
+        const { message, from, roomID} = messageObj;
+        //ZADD {room:1:3} 1615480369 {user:1, date:1615480369, message:'Hello"}
+        const date = Date.now();
+        //add this propr to messageObj
+        message.date = date;
+        console.log("MESSAGE OBJ: " + JSON.stringify(message));
+        //add user to online users //when is loggouted or session expired then remove it from this set
+        await sadd("online_users", message.from);
+
+        const roomKey = `room:${roomID}`;
+        //roomExist its same as hasMessage
+        const roomExists = await exists(roomKey);
+
+        if(!roomExists){
+        //or we have to create room and then send message
+        //ROOM WILL BE ONLY CREATED WHEN Client send message TO HOUSEWORKER and this houseworker doesn't have room 
+            //get usersID from roomID => roomID->1:2
+            const usersID = roomID.split(":");//[1,2]
+            // const usersMemeber = usersID.join(":"); //1:2
+            //its same as roomID
+            console.log("USERS IDDD: " + usersID + " MEMEBER: " + usersMemeber);
+            const user1ID = usersID[0]; //1
+            const user2ID = usersID[1]; //2
+            console.log("USERS: " + user1ID + "/ " + user2ID);
+            await sadd(`user:${user1ID}:rooms`, `${roomID}`)
+            await sadd(`user:${user2ID}:rooms`, `${roomID}`)
+
+            //for more then 2 userIDs
+            usersID.forEach(async(id)=>{
+                // await sadd(`user:${id}:rooms`, usersMemeber);
+                await sadd(`user:${id}:rooms`, roomID);
+                console.log(`user:${id}:rooms`, roomID);
+            })
+
+            //structrure to send for showroom notification
+            const roomNotification={
+                id: message.roomID,
+                names:[
+                    //name of users in room
+                    usersID.forEach(async(id)=>{
+                        await hmget(`user:${id}`, "username");
+                    })
+                ]
+            }
+            //send data to subscriber of room
+            //msg contains roomID and usernames in this room, in client(from) we will read roomID and show msg only in this room
+            publish('show.room', roomNotification);
+            //broadcast to every show.room,
+            //To all connected clients except the sender 
+            socket.broadcast.emit('show.room', roomNotification);
+        }
+        //ZADD roomKey:1:2 1617197047 { "From": "2", "Date": 1617197047, "Message": "Hello", "RoomId": "1:2" }
+        //ZADD Key=room:1:2 Score=1617197047 Value=obj
+        const stringMessage = JSON.stringify(messageObj);
+        await zadd(roomKey, date, stringMessage);
+        
+        publish('message', messageObj);
+        //notify all user in Room with roomKey emit with 'message' event
+        io.to(roomKey).emit("message", messageObj) //THIS WILL SEND SIGNAL TO CLIENT
+        //just send users in actual room
+    })
+
+    //Listen on disconect event
+    socket.on('disconnect', async()=>{
+        //TEST THIS
+        //take userId from session (socket session in this situation)
+        const userID = socket.request.session.user.username;
+        //remove user from online_users set
+        await srem("online_users", userID);
+        const status = {
+            ...socket.request.session.user,
+            online:false
+        }
+        //notify users that disconnection is done
+        publish("user.disconected", status);
+        socket.broadcast.emit("user.disconnected", status);
+
+    })
+
 })
+
+
+
+
+
+
+
 
 
 // io.use() allows you to specify a function that is called for every new, incoming socket.io connection. 
@@ -144,57 +264,6 @@ io.on('connection', (socket)=>{
 // io.use((socket, next)=>{
 
 // })
-//const dataNOtSplited = "username:Novak"
-//data.split(":") --- ["username", "Novak"]
-//data[0] -> "username", data[1] => "Novak"
-//How Data flows
-
-
-//SET for usernameKEy with userIDKey because we ony have username prop from Neo4j(without userID)
-//usernameKEy = `username:{$username}` ---username:Novak
-//userID is generated as total_users +1 
-//userKey = `user:{$userID}` user:1
-//set(usernameKey, userKey) --- username:Novak user:1   (KEY: username:"Novak" VALUE: user:"1")
-
-
-//HMSET --HASED SET Betweeen KEY: UserKEY, VALUES: Username and password
-//hmset(userKey, ["username", username, "password", hassedPassword])
-// ---- user:1 ["username" ,"Novak", "password", "Wjk1h28as8$!@1231312"]
-
-
-//ROOMS --- USER COULD HAVE ROOM (1 to 1) OR GROUP ROOMS (1 to many)
-//KEY---user:{$userID}:rooms,  VALUE---"{roomID}" 
-//user:{$userID}:rooms "{roomID}" // for 1 to 1 (room:1:3 ->room between user:1 and user:3) 
-//or Group chat (room:1 which 1 is a indetifier of group example(mainRoom -> 1), or room:2 (babysitters -> 2) )
-//SADD key member [member ...]
-//sadd(`user:{userID}:rooms`, `${roomID}`) --roomID is added to user with 
-//EXAMPLE ()
-//user:Novak has userID => user:1
-//user:Sara has userID => user:3
-//user:1:rooms romm:1:3  (room between user:1 and user:3 added to user:1 roomSET)
-//also user:2 must have room:1:3 in his roomSet
-//user:3:rooms room:1:3
-
-//OR GROUP ROOM
-//user:Vlada has userID =>user:6
-//user:Marko has userID =>user:11
-//user:Novak has userID =>user:1
-//add roomID to into usersRooms
-//user:6:rooms room:1:6:11 //That means room of 3 users
-//user:11:rooms room:1:6:11
-//user:1:rooms room:1:6:11
-//NOW user 6 send message to this Room (RoomID:1:6:11 Where KEY room:1:6:11)
-
-
-//HOW TO ADD MESSAGE TO ROOM
-//WHEN user:1 to user:2 send message this will be sent in room:1:3
-//But we have to know order sending/receiving message to show on proper order
-//we could use ZADD Set which will SORT message base on timestamp
-
-//ZADD {roomID} timestamp {from ,date, mesasge}
-//user:1 send message to user:3 --> room:1:3
-//ZADD {room:1:3} 1615480369 {user:1, date:1615480369, message:'Hello"}
-
 
 
 //express-session
