f8156a3adb1b5c4d49c64b0b24ab008307707d93
VeckovN <novaknolewow@gmail.com>
2023-07-26 16:36:12 +0200

    Custom hook created for user data and handlers

---
+++ client/src/hooks/useUser.js
@@ -0,0 +1,86 @@
+import {useState, useEffect} from 'react'
+
+//This handlers are repated in differt components
+const useUser =(initialState)=>{
+
+    const [data, setData] = useState(initialState);
+
+    const onChange = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(prev=> (
+            {
+                ...prev,
+                [name]:value
+            }
+        ))
+    }
+
+    const onImageChange = (event)=>{
+        const file = event.target.files[0];
+        setData(prev =>(
+            {
+                ...prev,
+                ["picture"]:file
+            }
+        ))
+        console.log(file.name);
+    }
+
+
+    const onChangeCity = (e) =>{
+        let city = e.value;
+        setData(prev=>(
+            {
+                ...prev,
+                ["city"]:city
+            }
+        ))
+    }
+
+    //Only for Client users
+    const onChangeProffesions = (e) =>{
+        let professionsArray;
+        professionsArray = Array.isArray(e) ? e.map(p => p.value): [];
+        setData(prev =>(
+            {
+                ...prev,
+                ["professions"]:professionsArray
+            }
+        ))
+    }
+
+
+    //Only for Houseworker users
+    const onChangeHouseworker = (event) =>{
+        const name = event.target.name;
+        let value;
+        if(name=="age")
+            value = parseInt(event.target.value);
+        
+        value = event.target.value;
+        setData(prev=> (
+            {
+                ...prev,
+                [name]:value // example first_name:"Novak"
+            }
+        ))
+    }
+
+    const onChangeInterest = (e) =>{
+        let professionsArray;
+        professionsArray = Array.isArray(e) ? e.map(p => p.value): [];        
+        setData(prev =>(
+            {
+                ...prev,
+                ["interests"]:professionsArray
+            }
+        ))
+    }
+
+
+    return {data, onChange, onChangeHouseworker, onImageChange, onChangeCity, onChangeProffesions, onChangeInterest}
+
+}
+
+export default useUser
---
+++ client/src/utils/options.js
@@ -0,0 +1,53 @@
+
+const city_options =[
+    {value:'Beograd', label:"Beograd"},
+    {value:'Novi Sad' , label:"Novi Sad"},
+    {value:'Nis' , label:"Nis"},
+    {value:'Kragujevac' , label:"Kragujevac"},
+    {value:'Subotica' , label:"Subotica"},
+    {value:'Leskovac' , label:"Leskovac"},
+    {value:'Pancevo' , label:"Pancevo"},
+    {value:'Cacak' , label:"Cacak"},
+    {value:'Krusevac' , label:"Krusevac"},
+    {value:'Kraljevo' , label:"Kraljevo"},
+    {value:'Novi Pazar' , label:"Novi Pazar"},
+    {value:'Smederevo' , label:"Smederevo"},
+    {value:'Uzice' , label:"Uzice"},
+    {value:'Valjevo' , label:"Valjevo"},
+    {value:'Vranje' , label:"Vranje"},
+    {value:'Sabac' , label:"Sabac"},
+    {value:'Sombor' , label:"Sombor"},
+    {value:'Pozarevac' , label:"Pozarevac"},
+    {value:'Pirot' , label:"Pirot"},
+    {value:'Zajecar' , label:"Zajecar"},
+    {value:'Bor' , label:"Bor"},
+    {value:'Kikinda' , label:"Kikinda"},
+    {value:'Sremska Mitrovica' , label:"Sremska Mitrovica"},
+    {value:'Jagodina' , label:"Jagodina"},
+    {value:'Vrsac' , label:"Vrsac"}
+]
+
+//interests_option for Client Context
+const profession_options = [
+    { value:'Cistac', label:"Cistac" },
+    { value:'Dadilja', label:"Dadilja" },
+    { value:'Kuvar', label:"Kuvar" },
+    { value:'Staratelj', label:"Staratelj" },
+    { value:'Sobarica', label:"Sobarica" },
+    { value:'Domacica', label:"Domacica" }
+]
+// const interests_options = [
+//     { value:'Cistac', label:"Cistac" },
+//     { value:'Dadilja', label:"Dadilja" },
+//     { value:'Kuvar', label:"Kuvar" },
+//     { value:'Staratelj', label:"Staratelj" },
+//     { value:'Sobarica', label:"Sobarica" },
+//     { value:'Domacica', label:"Domacica" }
+// ]
+
+
+export {
+    city_options, 
+    profession_options
+}
+
