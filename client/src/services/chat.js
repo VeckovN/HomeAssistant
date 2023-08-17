487d78a85390a7ac3ead37eb1ea4bac0c573c5d2
VeckovN <novaknolewow@gmail.com>
2023-08-17 11:58:45 +0200

    path fixed

--- client/src/services/chat.js
+++ client/src/services/chat.js
@@ -4,7 +4,7 @@ const BASE_URL = 'http://localhost:5000/api/'
 
 export const getUserRooms = async(username) =>{
     try{
-        const result = await axios.get(BASE_URL + `/chat/rooms/${username}`)
+        const result = await axios.get(BASE_URL + `chat/rooms/${username}`)
         const data = result.data;
         return data;
     }
@@ -15,7 +15,7 @@ export const getUserRooms = async(username) =>{
 
 export const getMessagesByRoomID = async(roomID) =>{
     try{
-        const result = await axios.get(BASE_URL + `/chat/room/${roomID}/messages?offset=0&size=50`)
+        const result = await axios.get(BASE_URL + `chat/room/${roomID}/messages?offset=0&size=50`)
         //MUST PARSE TO JSON BECASE WE GOT MESSAGES AS STRING JSON
         //const messages = JSON.parse(result.data);
         const messages = result.data;
@@ -29,7 +29,7 @@ export const getMessagesByRoomID = async(roomID) =>{
 
 export const deleteRoom = async(roomID) =>{
     try{
-        const result = await axios.post(BASE_URL +'/chat/room/delete', {roomID:roomID});
+        const result = await axios.post(BASE_URL +'chat/room/delete', {roomID:roomID});
         return result; //message 'Room sucessfully deleted'
     }
     catch(err){
@@ -43,7 +43,7 @@ export const addUserToRoom = async(roomInfo) =>{
         //   roomID
         //   newUsername
         //}
-        await axios.post( BASE_URL + '/chat/room/addUser',roomInfo);
+        await axios.post( BASE_URL + 'chat/room/addUser',roomInfo);
     }
     catch(err){
         console.log(err);
