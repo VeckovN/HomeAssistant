ccdc52ef5d61268bc09ec77793bdcc8294b38882
VeckovN <novaknolewow@gmail.com>
2023-08-18 10:14:10 +0200

    Authentication middleware added

--- api/routes/chat.js
+++ api/routes/chat.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {isLogged ,checkClient, checkHouseworker} = require('../middleware/checkLoggin.js')
 
 const {
     getMessages,
@@ -9,31 +10,25 @@ const {
     getConversationCount
 } = require('../controller/chatController'); 
 
-
-
 const router = express.Router();
 
 // "api/chat/"
 // router.get('/', getRoomId);
 
 
-
 // '/room/:id/messages/offset=0?size=50 ->>>> with req.query we can take offset and size value from URL
 //http://localhost:5000/api/chat/room/1:2/messages?offset=0&size=50
 
-router.get('/room/:id/messages', getMessages);
-router.post('/room/delete', deleteRoom);
-router.post('/sendMessage', sendMessage);
-router.post('/room/addUser', addUserToRoom);
-
+router.get('/room/:id/messages', isLogged, getMessages);
+router.post('/room/delete', checkClient, deleteRoom);
+router.post('/sendMessage', isLogged, sendMessage);
+router.post('/room/addUser', checkClient, addUserToRoom);
 
 router.get('/conversationCount/:userID', getConversationCount);
 
-
-
 //probelm with req.sessions
 // router.get('/rooms', getAllRooms);
-router.get('/rooms/:username', getAllRooms);
+router.get('/rooms/:username', isLogged, getAllRooms);
 // router.delete('room/:roomID', deleteRoom);
 
 module.exports = router;
