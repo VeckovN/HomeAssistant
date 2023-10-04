d9aca5119cf90322f7e11c21a41054b0aa1aa14e
VeckovN <novaknolewow@gmail.com>
2023-10-04 19:04:25 +0200

    Unnecessary comments removed

--- api/routes/chat.js
+++ api/routes/chat.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const {isLogged ,checkClient, checkHouseworker} = require('../middleware/checkLoggin.js')
-
 const {
     getMessages,
     getAllRooms,
@@ -8,13 +7,9 @@ const {
     addUserToRoom,
     getConversationCount
 } = require('../controller/chatController'); 
-
 const router = express.Router();
 
 // "api/chat/"
-// router.get('/', getRoomId);
-
-
 // '/room/:id/messages/offset=0?size=50 ->>>> with req.query we can take offset and size value from URL
 //http://localhost:5000/api/chat/room/1:2/messages?offset=0&size=50
 
@@ -22,10 +17,6 @@ router.get('/room/:id/messages', isLogged, getMessages);
 router.post('/room/delete', checkClient, deleteRoom);
 router.post('/room/addUser', checkClient, addUserToRoom);
 router.get('/conversationCount/:userID', getConversationCount);
-
-//probelm with req.sessions
-// router.get('/rooms', getAllRooms);
 router.get('/rooms/:username', isLogged, getAllRooms);
-// router.delete('room/:roomID', deleteRoom);
 
 module.exports = router;
