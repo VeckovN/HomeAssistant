9c8e6cc64fa52ab8f13c986945698c826249f748
VeckovN <novaknolewow@gmail.com>
2023-10-05 18:42:15 +0200

    socket listener moved to socket custom hook

--- client/src/components/Page/Home.js
+++ client/src/components/Page/Home.js
@@ -1,48 +1,17 @@
-import {useEffect } from "react";
 import ClientHome from "./Client/Home/ClientHome.js";
 import HouseworkerHome from './Houseworker/HouseworkerHome';
-import {Routes, Route} from 'react-router-dom';
-import {toast} from 'react-toastify';
 
-
-//CLIENT - Serach, Filter, HouseworkersCard(wiht paggination)
-//GUEST sees everything just like THE CLIENT but 
-// -can't see all information(Working hours, Rating) and cant send message and post comment
-//HOUSEWORKER - Has own view, GeneralMessage, shortcuts(Cards) to view Messages,Comments and Ratings. 
-
-
-//if there isn't type then is 100% Guest
-//type=['client','houseworker']
-const Home = ({socket, connected, user}) =>{
+const Home = ({socket, user}) =>{
     // const coo = Cookie.get("sessionLog");
     if(user === null)
         console.log("TESSSSSSSS");
 
-    let client;
-    if(user)
-        client = user.type === 'Client' ? true : false;
-
-    useEffect(()=>{
-        if(connected && user){
-            console.log("NOTIIIIIIIFYU")
-            socket.on("commentResponseNotify", data =>{
-                const commentObj = JSON.parse(data);
-                const houseworkerID  =commentObj.houseworkerID;
-                const clientCommented = commentObj.client;
-
-                if(user.userID == houseworkerID){
-                    console.log("HEEEEEE");
-                    toast.info("Dobio si komentar od " + clientCommented)
-                }
-            })
-        }
-    },[socket, user])
-    
+    const isClient = user && user.type === 'Client' ? true : false;
    
     return (
         <div>
-            {client || user===null
-                // in ClientHome limit what Guest can do and see
+            {isClient || user==null
+                //if isn't client then is it a Guest(limited Client Home Page view)
                 ? <ClientHome socket={socket} /> 
                 // If is authenticated as Houseworker 
                 : <HouseworkerHome/>
