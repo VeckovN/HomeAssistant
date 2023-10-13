3b26ca590ab7e03968fa1a138b78705d36e86052
VeckovN <novaknolewow@gmail.com>
2023-10-13 11:34:30 +0200

    react hook form implemented

--- client/src/components/Page/Login/Login.js
+++ client/src/components/Page/Login/Login.js
@@ -1,60 +1,90 @@
 //Adding session timeout in a page in Reactjs
 //https://stackoverflow.com/questions/50036145/adding-session-timeout-in-a-page-in-reactjs
-import {useEffect} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import useUser from '../../../hooks/useUser';
-import LoginForm from './LoginForm';
-import {login, reset} from '../../../store/auth-slice';
-import {toast} from 'react-toastify';
+import {useDispatch} from 'react-redux';
+import { useEffect } from 'react';
+import {login, reset as resetRedux} from '../../../store/auth-slice';
+import { useForm } from 'react-hook-form';
+import {zodResolver} from "@hookform/resolvers/zod";
+import { string, z } from "zod";
 
 import './Login.css';
+
+
+
+const schema = z.object({
+    username: string().min(4, {message:"Username must contain at least 4 characters"}),
+    password: string().min(1, {message:"Password is required"})
+})
 
 const Login = () =>{
     const initialState ={
         username:'',
         password:'',
     }
+    const {register, handleSubmit, reset, formState, formState:{isSubmitSuccessful}} = useForm({defaultValues:initialState, resolver: zodResolver(schema)});
+    const {errors} = formState; 
 
-    const {data:formData, onChange} = useUser(initialState);
-    const {username, password} = formData
-
-    const {user, loading, success, error, message} = useSelector( (state) => state.auth)
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-
+    const dispatch = useDispatch();   
+    //UseEffect for restarting value of state
     useEffect(()=>{
-        if(error)
-            toast.error(message,{
-                className:'toast-contact-message'
-            })
-
-        if(success || user){
-            navigate('/')
-            toast.success("You have succeessfully logged in",{
-                className:'toast-contact-message'
-            })
+        if(formState.isSubmitSuccessful){
+            reset({username:'', password:''})
         }
-        //This caused unnecessary component re-rendering
-        // dispatch(reset())
+    },[formState, reset, isSubmitSuccessful])
 
-    // },[user,message,error,success])
-    },[user,error,success])
-    //without user in dependecies navigation(on home page) after submition wont be executed
+    // const onSubmitHandlerNew = (e) =>{
+    //     e.preventDefault();
+    //     handleSubmitSave();
+    // } 
 
-    const onSubmit = (e)=>{
-        e.preventDefault();
-        dispatch(login({username, password}))
-        // dispatch(reset());
+    const onSubmitHandler = (formValues) =>{
+        dispatch(login(formValues));
+        dispatch(resetRedux());
     }
 
     return (
-        <LoginForm
-            username={username}
-            password={password}
-            onChange={onChange}
-            onSubmit={onSubmit}
-        />
+        <>
+            <div className ='login_container'>
+                <div className="login_context">
+                    <div className='login_welcome'>
+                        <h3>Welcome</h3>
+                        <div className='logo-h'>Home Assistant</div>
+                    </div>
+                    <form onSubmit={handleSubmit(onSubmitHandler)} className='login_form'>
+                        <div className='input_container'>
+                            <input
+                                className='input_field'
+                                type='text'
+                                placeholder='Enter a username'
+                                //register has ("username", onChange, onBlur and ref props) 
+                                {...register('username')}
+                                // name='username'
+                                // value={username}
+                                // onChange={onChange}
+                            />
+                            <div className="input_error">{errors.username?.message}</div>
+                        </div>
+                        
+
+                        <div className='input_container'>
+                            <input
+                                className='input_field'
+                                type='password'
+                                placeholder='Enter password'
+                                {...register('password')}
+                            />
+                             <div className="input_error">{errors.password?.message}</div>
+                        </div>
+
+                        <div className ='button_container'>
+                            <button type='submit' className='btn'>Log in</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+
+        </>
     )
 }
 
