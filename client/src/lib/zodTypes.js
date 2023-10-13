2647e9697a379cdad98daa857c2134c1d2dbbcf9
VeckovN <novaknolewow@gmail.com>
2023-10-13 11:40:24 +0200

    New Clients added

---
+++ client/src/lib/zodTypes.js
@@ -0,0 +1,40 @@
+import { string, z, array} from "zod"; 
+
+export const clientRegisterSchema = z.object({
+    username: 
+        string()
+        .min(1, "Username is required")
+        .min(5, "Username must contains at least 3 leters"),
+    firstName: 
+        string()
+        .min(1, "First name is required")
+        .min(3, "First name must contains at least 3 leters")
+        .refine(str=> /^[A-Z]/.test(str[0]), "First name must start with Capital latter"),
+    
+    lastName: 
+        string()
+        .min(1, "Last name is required")
+        .min(3, "First name must contains at least 3 leters")
+        .refine(str=> /^[A-Z]/.test(str[0]), "First name must start with Capital latter"),
+    
+    avatar: z.any()
+        .refine((file) => file?.size <= 300000, `Max image size is 3MB.`)
+        .refine((file) => ["image/jpeg", "image/jpg", "image/png", "image/webp"].includes(file?.type),
+        "Only .jpg, .jpeg, .png and .webp formats are supported."),
+    
+    email: string().email(),
+    // phone: string().transform(data => Number(data)),
+    password: 
+        string()
+        .min(1, "Password is required")
+        .min(4, "Username must contains at least 5 leters"),
+    confirmPassword: string().min(1, "Password is required"),
+    city: string().min(1, "City must be selected"),
+    gender: string().min(1, "Gender must be selected"),
+    interests: array(string()).min(1, "Interests must be selected"),
+}).refine(data => data.password === data.confirmPassword ,{
+    //cheking on matching password
+    message: "Password must match",
+    path:["confirmPassword"] //name of error if exists
+})
+
