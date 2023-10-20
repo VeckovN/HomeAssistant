92c2e65393bd677d992e0dee7629b018ec45130a
VeckovN <novaknolewow@gmail.com>
2023-10-20 18:03:15 +0200

    index.css file replaced with SCSS main file

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/index.js'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import './sass/style.scss'
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <Provider store={store} >
