f94375f5ec707f37d49f5d1bee375d89bcf0d658
VeckovN <novaknolewow@gmail.com>
2023-10-26 14:53:48 +0200

    container div changed

--- client/src/components/UI/CommentItem.js
+++ client/src/components/UI/CommentItem.js
@@ -4,9 +4,9 @@ import '../../sass/components/_commnetItem.scss';
 const CommentItem = (props) =>{
 
     return(
-        <div className='comment-container' key={props.id} >
+        <div className='comment' key={props.id} >
             {props.onDeleteCommentHandler && 
-                <button className='btn-comment-delete' onClick={(e) => props.onDeleteCommentHandler(e, props.id, props.from)}>X</button>
+                <button className='comment-delete-btn' onClick={(e) => props.onDeleteCommentHandler(e, props.id, props.from)}>X</button>
             }
             <div className='info'>
                 <div className='commented'>Commented: {props.from}</div>
