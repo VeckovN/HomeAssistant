f7483786e35d727ddd128df52ba844abe90e01c7
VeckovN <novaknolewow@gmail.com>
2023-10-27 15:50:16 +0200

    css replaced by sas

--- client/src/utils/FormInput.js
+++ client/src/utils/FormInput.js
@@ -1,16 +1,18 @@
+import '../sass/components/_formInput.scss';
+
 const FormInput = ({type, name, register, errors, placeholder, autoComplete}) =>{    
     return(
-        <>
+        <div className='form-input'>
             <input
-                className='input_field'
+                className='input-field'
                 type={type}
                 placeholder={placeholder || `Enter ${name}`}
                 {...register(name)}
                 autoComplete={autoComplete && autoComplete}
             />
             {/* <div className='input_errors'>{errors.errorsname?.message}</div> */}
-            <div className='input_errors'>{errors[name]?.message}</div>
-        </>
+            <div className='input-errors'>{errors[name]?.message}</div>
+        </div>
     )
 }
 
