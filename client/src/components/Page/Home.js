126d0bacc8c8ef953c56cdf65de53c3e275a4938
VeckovN <novaknolewow@gmail.com>
2023-11-03 11:43:17 +0100

    unnecessary div removed

--- client/src/components/Page/Home.js
+++ client/src/components/Page/Home.js
@@ -9,14 +9,14 @@ const Home = ({socket, user}) =>{
     const isClient = user && user.type === 'Client' ? true : false;
    
     return (
-        <div>
+        <>
             {isClient || user==null
                 //if isn't client then is it a Guest(limited Client Home Page view)
                 ? <ClientHome socket={socket} /> 
                 // If is authenticated as Houseworker 
                 : <HouseworkerHome/>
             }
-        </div>
+        </>
 
         
     )
