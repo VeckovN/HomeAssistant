eb1266ad1116ae3a1a096b8e870c4b42597810e0
VeckovN <novaknolewow@gmail.com>
2023-11-13 11:09:45 +0100

    multiple async functions merged in one

--- client/src/components/Page/Client/Home/Filter/Filter.js
+++ client/src/components/Page/Client/Home/Filter/Filter.js
@@ -13,29 +13,51 @@ const Filter = (prop) =>{
     const [cities, setCities] = useState();
     const [professions, setProfessions] = useState();
     const {data:filters, onChange, onChangeProffesions, onChangeCity} = useUser({});
-    
+   
+     //2 func merged in one async func
+     const fetchData = async () =>{
+        const professionsResult = await getAllProfessions();
+        const citiesResult = await getAllCities(); 
+
+        setProfessions(professionsResult);
+        setCities(citiesResult);
+    } 
+
     useEffect(()=>{
-       fetchCities()
-       fetchProfessions();
+        fetchData();
     },[])
 
-    //Fetch professions only one
-    
-    const fetchProfessions = async() => {
-        const professionsResult = await getAllProfessions();
-        console.log("PROFESSSION : " + JSON.stringify(professionsResult));
-        setProfessions(professionsResult);
-    }
+    // //Fetch professions only one
+    // const fetchProfessions = async() => {
+    //     const professionsResult = await getAllProfessions();
+    //     console.log("PROFESSSION : " + JSON.stringify(professionsResult));
+    //     setProfessions(professionsResult);
+    // }
 
-    const fetchCities = async() =>{
-        const citiesResult = await getAllCities();
-        setCities(citiesResult);
-    }
+    // const fetchCities = async() =>{
+    //     const citiesResult = await getAllCities();
+    //     setCities(citiesResult);
+    // }
+
+    // useEffect(()=>{
+    //     //This will trigger 2 times re-rendering
+    //     // fetchProfessions();
+    //     // fetchCities();
+
+    //     //However, if there is a dependency between the two (e.g., the result of one fetch is needed to perform the other),
+    //     //you might want to consider using Promise.all or chaining the promises to ensure a specific order of execution.
+    //     // // Promise.all([fetchCities(), fetchProfessions()]).then(() => {
+    //     // //     setLoading(false);
+    //     // //   });
+    //     // Promise.all([fetchCities(), fetchProfessions()]);
+    
+    // },[])
+
     
     //send filter options to parrent commpoent using prop.filterOptions ->passed funtion to this commponent
     const filterClickHanlder = (e) =>{
         prop.filterOptions(filters);
-        console.log("FILTER APPLIED: " + JSON.stringify(filters));
+        console.log("FILTER APPLIED: " +   JSON.stringify(filters));
     }
 
     //cities_options will be only recalcuated when is cities changed
