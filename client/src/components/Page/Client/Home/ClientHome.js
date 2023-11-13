65f1d19b531d8f73360e1c6d39dd0fcbf83f8f21
VeckovN <novaknolewow@gmail.com>
2023-11-13 13:17:30 +0100

    Scroll to Top button fix and designed

--- client/src/components/Page/Client/Home/ClientHome.js
+++ client/src/components/Page/Client/Home/ClientHome.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useCallback} from 'react'
 import useClient from '../../../../hooks/useClient.js';
 import HouseworkerCard from './HouseworkerCard/HouseworkerCard.js'
 import Filter from './Filter/Filter.js';
@@ -6,34 +6,36 @@ import { useSelector } from 'react-redux';
 import Search from './SearchAndSort/Search.js';
 import Sort from './SearchAndSort/Sort.js';
 import Spinner from '../../../UI/Spinner.js';
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
+
 
 import '../../../../sass/pages/_clientHome.scss';
 
 const ClientHome = ({socket}) =>{
+    console.log("clientHome Redner");
+    // alert("CL RP 1");
 
     //LIMIT IF IS GUEST 
     console.log("SOCKET " + socket);
 
     const [showButton ,setShowButton] = useState(false);
-    const {user} = useSelector((state) => state.auth)
+    const {user} = useSelector((state) => state.auth);
     const {data, loading, recommended, showRecommended, onShowRecommended, searchDataHanlder, filterDataHandler } = useClient(user);
-    // const [loading, setLoading] = useState(true);
+    const [houseworkerData, setHouseworkerData] = useState([]);
 
-    useEffect(()=>{
-        window.addEventListener('scroll', handleScroll);
-
-        return () =>{
-            window.removeEventListener('scroll', handleScroll);
-        }
-    }, []);
+    //alert("After; useClient")
+    // console.log("DATA: " , data);
 
     //event listener for showing button when is Y: +100px view
-    const handleScroll = () =>{
-        if(window.scrollY >= 1000)
+    const handleScroll = useCallback(() =>{
+        if(window.scrollY >= 1000){
             setShowButton(true);
-        else
+        }
+        else{            
             setShowButton(false)
-    }
+        }
+    },[]);
+
     
     const scrollToTop = () =>{
         window.scrollTo({
@@ -42,42 +44,26 @@ const ClientHome = ({socket}) =>{
           });
     }
 
+    useEffect(()=>{
+        window.addEventListener('scroll', handleScroll);
+        return () =>{
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, [handleScroll]); //put handleScroll as dependecies -> it will be applied only one times 
+    //because using useCallback its creating(freezing) only one copy of that function
 
-    //RECOMMENDED USERS
-    let recommendedList;
-    {recommended ? 
-        recommendedList = recommended.map(user =>
-            <>
-            {console.log("ID::: " + user.id)}
-            <HouseworkerCard
-                recommended={true}
-                socket={socket}
-                key={user.id}
-                id={user.id}
-                username={user.username}
-                email={user.email}
-                first_name={user.first_name}
-                last_name={user.last_name}
-                description={user.description}
-                picturePath={user.picturePath}
-                gender={user.gender}
-                city={user.city}
-                address={user.address}
-                age={user.age}
-                phone_number={user.phone_number}
-                professions={user.professions}
-            />
-            </>
-            )
-            : recommendedList =[]
-    }
+    console.log("DATA: " , data);
 
-    //HOUSEWORKER USERS
-    let houseworkerList;
-    {data ? 
+    //THIS CAUSED RE_RENDERING ON EVERY DATA CHANGE ITS CHANGE  setHouseworkerData(houseworkerList); THAT AGAIN CAUSE RE_REDNERING
+    useEffect(()=>{
+        //alert("useEffect(()=>{ let houseworkerList;")
+        let houseworkerList;
+        data  ? 
+        // data ? 
         houseworkerList = data.map(user =>
             <>
             {console.log("ID::: " + user.id)}
+            {/* {console.log("DATA: \n " + JSON.stringify(data))} */}
             <HouseworkerCard
                 recommended={false}
                 socket={socket}
@@ -98,8 +84,71 @@ const ClientHome = ({socket}) =>{
             />
             </>
             )
-            : houseworkerList =[]
-    }
+            : houseworkerList =[]       
+
+            if(houseworkerList?.length >0){
+                setHouseworkerData(houseworkerList);
+                //alert("setHouseworkerData(houseworkerList);");
+            }
+    },[data]);
+
+    // //RECOMMENDED USERS
+    // let recommendedList;
+    // {recommended ? 
+    //     recommendedList = recommended.map(user =>
+    //         <>
+    //         {console.log("ID::: " + user.id)}
+    //         <HouseworkerCard
+    //             recommended={true}
+    //             socket={socket}
+    //             key={user.id}
+    //             id={user.id}
+    //             username={user.username}
+    //             email={user.email}
+    //             first_name={user.first_name}
+    //             last_name={user.last_name}
+    //             description={user.description}
+    //             picturePath={user.picturePath}
+    //             gender={user.gender}
+    //             city={user.city}
+    //             address={user.address}
+    //             age={user.age}
+    //             phone_number={user.phone_number}
+    //             professions={user.professions}
+    //         />
+    //         </>
+    //         )
+    //         : recommendedList =[]
+    // }
+
+    // //HOUSEWORKER USERS
+    // let houseworkerList;
+    // {data ? 
+    //     houseworkerList = data.map(user =>
+    //         <>
+    //         {console.log("ID::: " + user.id)}
+    //         <HouseworkerCard
+    //             recommended={false}
+    //             socket={socket}
+    //             key={user.id}
+    //             id={user.id}
+    //             username={user.username}
+    //             email={user.email}
+    //             first_name={user.first_name}
+    //             last_name={user.last_name}
+    //             description={user.description}
+    //             picturePath={user.picturePath}
+    //             gender={user.gender}
+    //             city={user.city}
+    //             address={user.address}
+    //             age={user.age}
+    //             phone_number={user.phone_number}
+    //             professions={user.professions}
+    //         />
+    //         </>
+    //         )
+    //         : houseworkerList =[]
+    // }
 
     return (
         <div className='home-container'>
@@ -116,14 +165,15 @@ const ClientHome = ({socket}) =>{
                     />
                 </div>
                 
-                <button className="recommended-btn" onClick={onShowRecommended}>{!showRecommended ? 'Show recommended' : 'Close Recommended'}</button>
+                {/* <button className="recommended-btn" onClick={onShowRecommended}>{!showRecommended ? 'Show recommended' : 'Close Recommended'}</button> */}
                 
                 
                 <div className="houseworker-list">    
                     {loading ? <Spinner/> :
                     <>
-                        {showRecommended && recommendedList }
-                        {houseworkerList.length > 0 ? houseworkerList : <h3 id='none'>No Matches</h3> }
+                        {/* {showRecommended && recommendedList } */}
+                        {/* {houseworkerList.length > 0 ? houseworkerList : <h3 id='none'>No Matches</h3> } */}
+                        {houseworkerData.length > 0 ? houseworkerData : <h3 id='none'>No Matches</h3> }
                     </>
                     }
                 </div>   
@@ -132,7 +182,7 @@ const ClientHome = ({socket}) =>{
 
             {showButton && 
                 <div className='scroll-div'>
-                    <button className='scroll-to-top' onClick={scrollToTop}>Scroll To Top</button>
+                    <button id='scroll-to-top' onClick={scrollToTop}><KeyboardDoubleArrowUpIcon/></button>
                 </div>
             }
         </div>
