e3cdcb36989c8cf4317d8614d56653df922f5877
VeckovN <novaknolewow@gmail.com>
2023-11-19 16:56:29 +0100

    PhotoWithHover component implemented

--- client/src/components/Page/Messages/Rooms/Room/Room.js
+++ client/src/components/Page/Messages/Rooms/Room/Room.js
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 
 import '../../../../../sass/components/_room.scss';
+import PhotoWithHover from '../../../../../utils/PhotoWithHover';
 
 const Room = ({roomInfo, user, houseworkers, roomRef, onRoomClickHanlder, onAddUserToGroupHanlder, onDeleteRoomHandler}) =>{
 
@@ -28,6 +29,7 @@ const Room = ({roomInfo, user, houseworkers, roomRef, onRoomClickHanlder, onAddU
         onAddUserToGroupHanlder(roomID, selectedUsername)
     }
 
+
     // console.log("ROOM INFO . USERS LENGTH: " + roomInfo.users.length);
     //console.log("RommINfo", roomInfo);
 
@@ -44,14 +46,11 @@ const Room = ({roomInfo, user, houseworkers, roomRef, onRoomClickHanlder, onAddU
                     return(
                     <>
                         <div className='group-sign'>Group</div>
-                        <div className="photo" style={{backgroundImage: "url(https://i.pinimg.com/originals/a9/26/52/a926525d966c9479c18d3b4f8e64b434.jpg)"}}>
-                            <div className="online">{user}</div>
-                            {/* <div className="online"></div> */}
-                        </div>
-                        {/* <div class="desc-contact">
-                            <p class="name">Dave Corlew</p>
-                            <p class="message">Let's meet for a coffee or something today ?</p>
-                        </div> */}
+
+                        <PhotoWithHover
+                            url="url(https://i.pinimg.com/originals/a9/26/52/a926525d966c9479c18d3b4f8e64b434.jpg)"
+                            user={user}
+                        />
                     </>
                     )
                 })}
@@ -68,16 +67,19 @@ const Room = ({roomInfo, user, houseworkers, roomRef, onRoomClickHanlder, onAddU
             {/* <button className={`room ${roomInfo.roomID === roomInfo.roomID == roomRef.current && 'active'}`} value={roomInfo.roomID} ref={roomRef} onClick={onRoomClickHanlder}> */}
                 {console.log("USER: aaaaa " , roomInfo)}
                 {/* IF PRIVATE THAT SHOW PROFILE PICTURE WITH NAME DESK */}
-                <div className="photo" style={{backgroundImage: "url(https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80)"}}>
-                {/* <div className="photo" src={`assets/userImages/${houseworkerProps.picturePath}`}> */}
-                    {/* Green dot for online user */}
-                    <div className="online"></div> 
-                </div>
-                <div className="room-contact">
-                    <p className="name">Name: {roomInfo.users[0]}</p>
-                    {/* last message */}
-                    {/* <p className="message">9 pm at the bar if possible 😳</p> */}
-                    <p className="message">Last message</p>
+               
+                <div className='room-info'>
+                    <div className="photo" style={{backgroundImage: "url(https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80)"}}>
+                    {/* <div className="photo" src={`assets/userImages/${houseworkerProps.picturePath}`}> */}
+                        {/* Green dot for online user */}
+                        <div className="online"></div> 
+                    </div>
+                    <div className="room-contact">
+                        <p className="name">Name: {roomInfo.users[0]}</p>
+                        {/* last message */}
+                        {/* <p className="message">9 pm at the bar if possible 😳</p> */}
+                        <p className="message">Last message</p>
+                    </div>
                 </div>
                 {/* timer for the last received message */}
                 <div className="timer">12 sec</div>
