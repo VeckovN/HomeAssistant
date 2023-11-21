6da0d27a92a092e635e4f2df2a4f4e0425fa4b41
VeckovN <novaknolewow@gmail.com>
2023-11-21 12:20:52 +0100

    Unnecessary comments deleted

--- client/src/components/Page/Messages/Rooms/Rooms.js
+++ client/src/components/Page/Messages/Rooms/Rooms.js
@@ -1,32 +1,9 @@
 import Spinner from "../../../UI/Spinner";
 import Room from "./Room/Room.js";
 
-import '../../../../sass/components/_rooms.scss';
-
 const Rooms = ({rooms, roomRef, user, houseworkers, onAddUserToGroupHanlder, onDeleteRoomHandler, onRoomClickHanlder}) =>{
 
     return (
-        // <div className='room-conainter'>
-        //         {rooms ?
-        //             rooms.map((el, index)=>(      
-        //             <div className='rooms' key={el.roomID}><span className='room-label'></span>
-        //                 <Room
-        //                     roomInfo={el}
-        //                     user={user}
-        //                     roomRef={roomRef}
-        //                     houseworkers={houseworkers}
-        //                     onAddUserToGroupHanlder={onAddUserToGroupHanlder}
-        //                     onDeleteRoomHandler={onDeleteRoomHandler}
-        //                     onRoomClickHanlder={onRoomClickHanlder}
-        //                 />
-        //             </div>
-        //             ))
-        //         : <Spinner></Spinner>
-        //         }
-                    
-        //         {rooms.length==0 && <div className='no_rooms'>You have no conversation</div>}
-                
-        //     </div>
             <>
                 {rooms ?
                     rooms.map((el, index)=>(      
@@ -42,9 +19,7 @@ const Rooms = ({rooms, roomRef, user, houseworkers, onAddUserToGroupHanlder, onD
                     ))
                 : <Spinner></Spinner>
                 }
-                    
                 {rooms.length==0 && <div className='no_rooms'>You have no conversation</div>}
-
             </>
     )
 }
