2d01f0c3c619c8afbdb79a5c571a0f6438acebaf
VeckovN <novaknolewow@gmail.com>
2023-12-01 08:38:07 +0100

    override prop chaged

--- client/src/components/UI/Spinner.js
+++ client/src/components/UI/Spinner.js
@@ -4,9 +4,10 @@ const override = {
     display: "block",
     margin : "0 auto",
 }
-const Spinner = () =>{
+const Spinner = ({className}) =>{
     return (
-        <div className='spinner_container'>
+        <div className={className}>
+        {/* <div className='spinner'> */}
             <ClockLoader
                 color="rgba(180, 240, 250, 1)"
                 cssOverride={override}
