3bdbf05c910e377ff1beffbb9321b95876fd0f95
VeckovN <novaknolewow@gmail.com>
2023-12-02 10:40:06 +0100

    added new container of paragraph and div

--- client/src/components/Page/Client/Home/Filter/FilterForm.js
+++ client/src/components/Page/Client/Home/Filter/FilterForm.js
@@ -2,51 +2,60 @@ import Select from 'react-select';
 
 const FilterForm = ({city_options, profession_options, onChange, onChangeCity, onChangeProffesions, filterClickHanlder }) =>{
     return (
-        <div className='filterBox'>
+        <div className='filter-box'>
 
-            <div className='select-filter-box'>
-                <Select 
-                    className='filter-select'
-                    placeholder="Select profession"
-                    options={profession_options}
-                    onChange={onChangeProffesions}
-                    isMulti
-                    isClearable
-                />
-            </div>
-
-            <label className='filter-lb'>City:</label>
-            <div className='filter-card'>   
-                <Select 
-                    className='filter-select'
-                    placeholder="Select a city"
-                    options={city_options}
-                    onChange={onChangeCity}
-                    isClearable
-                />
-            </div>
-
-            <label className='filter-lb'>Gender:</label>
-            <div className='filter-card'>
-                <div className='filter-item'>
-                    <input type="radio"  onChange={onChange} name="gender" value="Male"/>
-                    <label >Male</label><br/>
-                </div>
-                <div className='filter-item'>
-                    <input type="radio" onChange={onChange} name="gender" value="Female"/>
-                    <label >Female</label><br/>
+            <div className='filter-box-item'>
+                <label className='filter-lb'>Professions:</label>
+                <div className='select-filter-box'>
+                    <Select 
+                        className='filter-select'
+                        placeholder="Select profession"
+                        options={profession_options}
+                        onChange={onChangeProffesions}
+                        isMulti
+                        isClearable
+                    />
                 </div>
             </div>
 
-            <label className='filter-lb'>Age:</label>
-            <div className='filter-card'>
-                <div className='filter-item'>
-                    <label>From</label>
-                    <input className='age-input' type='number' onChange={onChange} name='ageFrom'/>
+            <div className='filter-box-item'>
+                <label className='filter-lb'>City:</label>
+                <div className='filter-card'>   
+                    <Select 
+                        className='filter-select'
+                        placeholder="Select a city"
+                        options={city_options}
+                        onChange={onChangeCity}
+                        isClearable
+                    />
                 </div>
-                <div className='filter-item'>
-                    <label>To</label>
-                    <input className='age-input' type='number' onChange={onChange} name='ageTo'/>
+            </div>
+
+            <div className='filter-box-item'>
+                <label className='filter-lb'>Gender:</label>
+                <div className='filter-card'>
+                    <div className='filter-item'>
+                        <input type="radio"  onChange={onChange} name="gender" value="Male"/>
+                        <label >Male</label><br/>
+                    </div>
+                    <div className='filter-item'>
+                        <input type="radio" onChange={onChange} name="gender" value="Female"/>
+                        <label >Female</label><br/>
+                    </div>
+                </div>
+            </div>
+
+            <div className='filter-box-item'>
+                <label className='filter-lb'>Age:</label>
+                <div className='filter-card'>
+                    <div className='filter-item'>
+                        <label>From</label>
+                        <input className='age-input' type='number' onChange={onChange} name='ageFrom'/>
+                    </div>
+                    <div className='filter-item'>
+                        <label>To</label>
+                        <input className='age-input' type='number' onChange={onChange} name='ageTo'/>
+                    </div>
                 </div>
             </div>
 
