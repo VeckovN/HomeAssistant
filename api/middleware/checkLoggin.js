f48258b629f1572cff22de45f9988fe5728a4948
VeckovN <novaknolewow@gmail.com>
2023-12-22 12:53:35 +0100

    extend Session cookie max age implemented

--- api/middleware/checkLoggin.js
+++ api/middleware/checkLoggin.js
@@ -1,4 +1,10 @@
-
+const extendSessionCookieMaxAge = (req,res,maxAge) =>{
+    // const newMaxAge = 1000 * 60 * 10;
+    const newMaxAge = maxAge;
+    const currentCookie = req.cookies.sessionLog;
+    // Set the updated cookie in the response headers
+    res.cookie('sessionLog', currentCookie, { maxAge: newMaxAge });
+}
 
 //authentication check regardless of the type of user
 const isLogged = (req,res,next)=>{
@@ -8,26 +14,28 @@ const isLogged = (req,res,next)=>{
         })
     }
     //if is authoriazied just move on other middleware()
+    const maxAge = 1000 * 60 * 10;
+    extendSessionCookieMaxAge(req,res,maxAge);
+
     next();
 }
 
 
 const checkClient = (req,res,next)=>{
-    console.log("Start");
-    console.log("SESSION: " + JSON.stringify(req.session))
-
     if(!req.session.user){
         //if is authoriazied just move to the other middleware()
         console.log("User isn't authenticated");
         return res.json({error:"User isn't authenticated"});        
     }
         
-    console.log("checkClient");
-    if(req.session.user.type === 'Client')
+    if(req.session.user.type === 'Client'){
+        const maxAge = 1000 * 60 * 10;
+        extendSessionCookieMaxAge(req,res,maxAge);
+        
         next();
+    }    
     else
     {
-        console.log("TES::::::::");
         return res.json({error:"You're not authorized"})
     }
         
@@ -36,12 +44,15 @@ const checkClient = (req,res,next)=>{
 const checkHouseworker = (req,res,next)=>{
 
     if(!req.session.user)
-        //if is authoriazied just move on other middleware()
         return res.json({error:"User isn't authenticated"})
         //return new Error({message:"Not Authenticated"});
 
-    if(req.session.user.type === 'Houseworker')
+    if(req.session.user.type === 'Houseworker'){
+        const maxAge = 1000 * 60 * 10;
+        extendSessionCookieMaxAge(req,res,maxAge)
+
         next();
+    }
     else
         return res.json({error:"You're not authorized"})
 }
