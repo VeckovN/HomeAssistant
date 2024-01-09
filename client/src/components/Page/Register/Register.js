0bae5aa556537642536ecdb4b880bcdbd424c60a
VeckovN <novaknolewow@gmail.com>
2024-01-09 20:42:09 +0100

    Register page redesigned

--- client/src/components/Page/Register/Register.js
+++ client/src/components/Page/Register/Register.js
@@ -7,12 +7,25 @@ const Register = () =>{
     return (
         <div className='register-container'>
             <div className='register-options'>
+
                 <div className='register-item'>
-                    <Link to='/clientRegister' className='register-link'>Client</Link>
+                    <Link to='/clientRegister' className='register-link'>
+                        <div className='avatar-container'>
+                            <img src='assets/clientAvatar1.png'></img>
+                        </div>
+                        <div className='register-label'>Client</div>
+                    </Link>
                 </div>
+
                 <div className='register-item'>
-                    <Link to='/houseworkerRegister' className='register-link'>Houseworker</Link>
+                    <Link to='/houseworkerRegister' className='register-link'>
+                        <div className='avatar-container'>
+                            <img src='assets/houseworkerAvatar1.png'></img>
+                        </div>
+                        <div className='register-label'>Houseworker</div>
+                    </Link>
                 </div>
+
             </div>
         </div>
 
