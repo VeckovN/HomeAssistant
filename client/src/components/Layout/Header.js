82e6ec96a67e3cfba0965d892fd983f1c02c501f
VeckovN <novaknolewow@gmail.com>
2024-01-22 12:12:13 +0100

    responsive added

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import {Link, useNavigate} from 'react-router-dom';
 import {useSelector, useDispatch} from 'react-redux';
 import {logout, reset} from '../../store/auth-slice';
-import { useEffect } from 'react';
+import { useEffect , useState} from 'react';
 import {toast} from 'react-toastify';
 
 import '../../sass/layout/_header.scss';
@@ -9,6 +9,7 @@ import '../../sass/layout/_header.scss';
 const Header = () =>{
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
 
     const {user, message, error, success} = useSelector((state) => state.auth) //null when not exists
     let houseworker;
@@ -19,7 +20,8 @@ const Header = () =>{
 
     const logoutHandler = () =>{
         //emit disconnected socket
-        dispatch(logout())
+        dispatch(logout());
+        setShowMenu(false);
     }
 
     useEffect(()=>{
@@ -35,41 +37,80 @@ const Header = () =>{
             })
         }
     },[user,error,success])
-    
+
+
+    const removeShowMenuHandler = () =>{
+        setShowMenu(false);
+    }
+
 
   return (
-    <nav className = "navbar">
+    <nav className = {`navbar ${showMenu ? 'navbar-mobile' : ''}`}>
         <div className = "logo" >
             {/* <a href="/" className="logo-a"> */}
-            <Link to="/" className='logo-a'>
+            <Link to="/" className='logo-a' onClick={removeShowMenuHandler}>
                 <span className='logo-span'>Home</span> 
                 <span className="logo-span">Assistant</span>
             {/* </a> */}
             </Link>
         </div>
-        <div className="nav-list">
+        
+
+        <div className={`burger ${showMenu ? 'burger-active' : ''}`} onClick={()=> setShowMenu(!showMenu)}>
+            <span className='bar'></span>
+            <span className='bar'></span>
+            <span className='bar'></span>
+        </div>
+
+        <div className={`nav-list ${showMenu ? 'activeList' : ''}`}>
             {/* unAuthenticated users */}
             {!user ? (
-                        <>
-                            <Link to='/login' className='nav-link'>Login</Link>
-                            <Link to='/register' className='nav-link'>Register</Link>
-                        </>
-                    ):
-                        <>
-                            <Link to='/profile' className='nav-link'>Profile</Link>
-                            <Link to='/messages' className='nav-link'>Messages</Link>
-                        
+              
+                //don't set onClick event when the menu isn't clicked
+                !showMenu ? 
+                    <>
+                        <Link to='/login' className='nav-link' >Login</Link>
+                        <Link to='/register' className='nav-link' >Register</Link>
+                    </>
+                :
+                    <>
+                        <Link to='/login' className='nav-link' onClick={removeShowMenuHandler}>Login</Link>
+                        <Link to='/register' className='nav-link' onClick={removeShowMenuHandler}>Register</Link>
+                    </>
+                
+            ):
+                !showMenu ?
+                <>
+                    <Link to='/profile' className='nav-link' >Profile</Link>
+                    <Link to='/messages' className='nav-link' >Messages</Link>
+                
 
-                            {/* if is houseworker then show Comment LInk*/}
-                            {houseworker &&
-                                <Link to='/comments' className='nav-link'>Comments</Link>
-                            }
+                    {/* if is houseworker then show Comment LInk*/}
+                    {houseworker &&
+                        <Link to='/comments' className='nav-link' >Comments</Link>
+                    }
 
-                            <button onClick={logoutHandler} className='nav-link-button '>Logout</button>
-                                
-                        </>
+                    <button onClick={logoutHandler} className='nav-link-button '>Logout</button>
+                </>
+                :
+
+                <>
+                    <Link to='/profile' className='nav-link' onClick={removeShowMenuHandler}>Profile</Link>
+                    <Link to='/messages' className='nav-link' onClick={removeShowMenuHandler}>Messages</Link>
+                
+
+                    {/* if is houseworker then show Comment LInk*/}
+                    {houseworker &&
+                        <Link to='/comments' className='nav-link' onClick={removeShowMenuHandler}>Comments</Link>
+                    }
+
+                    <button onClick={logoutHandler} className='nav-link-button '>Logout</button>
+                </>
+                
             }
+
         </div>
+        
     </nav>
     )
 }
