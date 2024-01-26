3f9a870e2a262fbdaf1f5aa87fc33dea2c209568
VeckovN <novaknolewow@gmail.com>
2024-01-26 18:51:06 +0100

    403 status added for not authorized requests

--- api/middleware/checkLoggin.js
+++ api/middleware/checkLoggin.js
@@ -13,9 +13,8 @@ const isLogged = (req,res,next)=>{
             error:"User isn't authenticated"
         })
     }
-    //if is authoriazied just move on other middleware()
-    extendSessionCookieMaxAge(req,res);
 
+    extendSessionCookieMaxAge(req,res);
     next();
 }
 
@@ -26,28 +25,36 @@ const checkClient = (req,res,next)=>{
         console.log("User isn't authenticated");
         return res.status(401).json({error:"User isn't authenticated"});        
     }
+
+    //Etc if logged Houseworker try to access Client endpoint- return 403 status
+    //and handle redirecting to login page(still loggedIn) we don't want to loggout this user
+    if(req.session.user.type === 'Houseworker'){
+        console.log("User ins't auhtorized");
+        return res.status(403).json({error:"Forbidden for you"});
+    }
         
     if(req.session.user.type === 'Client'){
         extendSessionCookieMaxAge(req,res);
-        
         next();
     }    
     else
     {
         return res.status(401).json({error:"You're not authorized"})
     }
-        
 }
 
 const checkHouseworker = (req,res,next)=>{
 
     if(!req.session.user)
         return res.status(401).json({error:"User isn't authenticated"})
-        //return new Error({message:"Not Authenticated"});
+
+    if(req.session.user.type === 'Client'){
+        console.log("User ins't auhtorized");
+        return res.status(403).json({error:"Forbidden for you"});
+    }
 
     if(req.session.user.type === 'Houseworker'){
         extendSessionCookieMaxAge(req,res)
-
         next();
     }
     else
