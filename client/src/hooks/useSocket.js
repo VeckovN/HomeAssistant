e798bad53b40c1cf95a7974399dfaa4a3effdb51
VeckovN <novaknolewow@gmail.com>
2024-07-04 18:32:52 +0200

    fixing socket

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -10,60 +10,116 @@ const useSocket = (user) =>{
     const [connected, setConnected] = useState(false);
 
     //on every useSocket will be this reRun
+    // useEffect(()=>{
+    //     if(user){
+    //         //if is socket initializated
+    //         if(socket !== null){
+    //             console.log("socket.connect();")
+    //             socket.connect();
+
+    //             //message send to all users and user which id is in message obj should
+    //             listenFormMessage(socket, user.userID);
+    //             //listen only for comment that belongs to logged user
+    //             listenForCommentNotification(socket, user.userID);
+    //             listenForRatingNotfication(socket, user.userID);
+
+    //         }
+    //         else{
+    //             const socketIn = io("http://127.0.0.1:5000", {
+    //                 withCredentials: true,
+    //             });
+    //             socketRef.current = socketIn
+    //             console.log("socketRef.current = socketIn")
+    //         }
+    //     setConnected(true);
+    //     }   
+    //     //userNotExxists (Not authorized)
+    //     else{
+    //         if(socket !== null){
+    //             socket.disconnect();
+    //             console.log("SOCKET DISCONNECTED")
+    //             setConnected(false);
+    //         }
+    //     }
+
+    //     return () => {
+    //         // socket.disconnect();
+            
+    //       };
+
+    // // },[socket,user]) //wont reacreate socket on change(socket)
+    // },[user, socket]) 
+
+
     useEffect(()=>{
         if(user){
-            //if is socket initializated
-            if(socket !== null){
-                console.log("socket.connect();")
-                socket.connect();
-
-                //message send to all users and user which id is in message obj should
-                listenFormMessage(socket, user.userID);
-                //listen only for comment that belongs to logged user
-                listenForCommentNotification(socket, user.userID);
-                listenForRatingNotfication(socket, user.userID);
-
-            }
-            else{
+            //if is socket not initializated
+            if(!socket){
                 const socketIn = io("http://127.0.0.1:5000", {
                     withCredentials: true,
                 });
                 socketRef.current = socketIn
                 console.log("socketRef.current = socketIn")
+
+                socketIn.on("connect", () =>{
+                    setConnected(true);
+                    console.log("Socket Connected After Initial");
+
+                    
+                    // //in both situations set again listeners
+                    // //message send to all users and user which id is in message obj should
+                    // listenFormMessage(socketRef.current, user.userID);
+                    // //listen only for comment that belongs to logged user
+                    // listenForCommentNotification(socketRef.current, user.userID);
+                    // listenForRatingNotfication(socketRef.current, user.userID);
+                })
+
+                socketIn.on("disconnect", ()=>{
+                    setConnected(false);
+                    console.log("Socket Disconnected");
+                })
+
+                
+                //in both situations set again listeners
+                //message send to all users and user which id is in message obj should
+                listenFormMessage(socketRef.current, user.userID);
+                //listen only for comment that belongs to logged user
+                listenForCommentNotification(socketRef.current, user.userID);
+                listenForRatingNotfication(socketRef.current, user.userID);
+
+                // return() =>{
+                //     socketIn.disconnect();
+                //     console.log('socketIn.disconnect()');
+                // }
             }
-        setConnected(true);
+            else{
+                // Reconnect the existing socket(when enter app and user is still authenticated)
+                socket.connect();
+                
+                // //in both situations set again listeners
+                // //message send to all users and user which id is in message obj should
+                // listenFormMessage(socket, user.userID);
+                // //listen only for comment that belongs to logged user
+                // listenForCommentNotification(socket, user.userID);
+                // listenForRatingNotfication(socket, user.userID);
+                console.log("socket.connect() - Reconnected")
+            }
+
+            // setConnected(true);
+
+            // return() =>{
+            //     socket.disconnect();
+            //     console.log('socketIn.disconnect();');
+            // }
         }   
-        //userNotExxists (Not authorized)
-        else{
-            // console.log("HHHEEEEEE ELSEEEEEEEEEE");
-            if(socket !== null){
-                // console.log("socket.disconnect();")
-                socket.disconnect();
-            }
-            // console.log("setConnected(false);")
-        }
+        //userNotExxists(Not authorized) and socket still esists
+        else if (socketRef.current){
+            socket.disconnect();
+            console.log("socketRef.current.disconnect()");
+        }   
 
-        // return () => {
-        //     socket.disconnect();
-        //   };
-
-    },[socket,user])
-
-    useEffect(()=>{
-
-        if(connected && user){
-
-        }
-        else{
-            if(socket){
-                socket.off("user.connected");
-                socket.off("message");
-                socket.off("user.room");
-            }
-        }
-
-    },[])
-
+    // },[socket,user]) //wont reacreate socket on change(socket)
+    },[user]) 
 
     //return socketOBj, and connected State
     return [socket,connected];
