865543f17e09ce37de1900f84742872238dac9d4
VeckovN <novaknolewow@gmail.com>
2024-10-21 13:08:48 +0200

    Add additional createRoomNotification prop to emitMessage data

--- client/src/utils/MessageUtils/handleMessage.js
+++ client/src/utils/MessageUtils/handleMessage.js
@@ -3,8 +3,14 @@ import { sendMessageToUser } from "../../services/chat";
 
 export const sendMessage = async(socket, messageObj) =>{
     const result = await sendMessageToUser(messageObj);
-    const {roomKey, dateFormat, lastMessage, unreadMessArray} = result;
-    const messageWithRoomKey = {...messageObj, roomKey:roomKey, date:dateFormat, lastMessage:lastMessage, unreadMessArray:unreadMessArray};
-    
+    const {roomKey, dateFormat, lastMessage, unreadMessArray, createRoomNotification} = result;
+    const messageWithRoomKey = {
+        ...messageObj, 
+        roomKey, 
+        date:dateFormat, 
+        lastMessage, 
+        unreadMessArray, 
+        createRoomNotification
+    };
     emitMessage(socket, {data:messageWithRoomKey});
 }
