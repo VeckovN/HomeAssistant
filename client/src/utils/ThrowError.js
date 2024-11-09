586438c9b19cfdbc3f5c3cadf153f640cc11d55b
VeckovN <novaknolewow@gmail.com>
2024-11-09 11:18:13 +0100

    Change getErrorMessage function for taking error object on error throwing

--- client/src/utils/ThrowError.js
+++ client/src/utils/ThrowError.js
@@ -5,19 +5,27 @@ export const ThrowErorr = (error) =>{
         //error.response know without .data.error etc Request faiuld with status code 404' 
     }
     else if(error.request){
-        throw new Error("Network error");
+        throw new Error("Network error. Please check your connection.");
         //this create new error with custom message
     }
     else 
-        throw new Error(error.message);
+        throw new Error(error.message || "An unknon wrror occurred.");
     
 }
 //controllers send res.status(404).json({error:'Comments Count error'});
 //with error.response. we got this json object and send 
 
 export const getErrorMessage = (err) =>{
-    if(err.response.data && err.response.data.error)
-        return {messageError:err.response.data.error};
-    
-    return err.message || err;
+    if(err.response?.data?.error){
+        // Specific error message from server response
+        return {messagError: err.response.data.error};
+    }
+    else if (err.message){
+        //General error messsage from error object
+        return {messagError: err.message};
+    }
+    else{
+        //Fallback message if no specific error is found
+        return {messagError: "An error occurred. Please try again later."}
+    }
 }
