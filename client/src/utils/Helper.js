b1c9760bef9cffcf0e131dafe59a3fe3657f7564
VeckovN <novaknolewow@gmail.com>
2024-11-19 11:16:07 +0100

    Fix getRoomId In order function and refactor it

--- client/src/utils/Helper.js
+++ client/src/utils/Helper.js
@@ -1,8 +1,5 @@
-// Format room ID to start with the lower user ID value
 export const getRoomIdInOrder = (firstUserID, secondUserID) =>{
-    // The room ID should be formatted as minID:maxID (e.g., room:1:2, room:4:9, not room:8:2)
-   const minUserID = firstUserID > secondUserID ? secondUserID : firstUserID;
-   const maxUserID = firstUserID > secondUserID ? firstUserID : secondUserID;
-   // Generate the room ID in the format minID:maxID between two users
-   return `${minUserID}:${maxUserID}`
+    const userIDs = [Number(firstUserID), Number(secondUserID)];
+    const [minUserID, maxUserID] = userIDs.sort((a,b) => a - b);
+    return `${minUserID}:${maxUserID}`;
 }
