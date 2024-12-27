cbad6958f02f79feb759f3bd8062178b172bfde4
VeckovN <novaknolewow@gmail.com>
2024-12-27 12:30:44 +0100

    Add demo accounts

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -67,10 +67,29 @@ const Login = () =>{
                     </picture>
 
                     <div className='context-item'>
+
                         <div className='login_welcome'>
                             <h3>Welcome</h3>
                             <div className='logo-h'>Home Assistant</div>
                         </div>
+
+                        <div className='demo_accounts'>
+                            <div className='demo_accounts-title'>Demo Accounts</div>
+                            <div className='accounts'>
+                                <div className='account-item'>
+                                    <div className='account-item-type'>Client</div>
+                                    <div>Username: <span>Veckov</span></div>
+                                    <div>Password: <span>veckov</span></div>
+                                </div>
+                                <div>
+                                    <div className='account-item-type'>Houseworker</div>
+                                    <div>Username: <span>Sara22</span></div>
+                                    <div>Password: <span>sara22</span></div>
+                                </div>
+                            </div>
+                        </div>
+
+
                         <form onSubmit={handleSubmit(onSubmitHandler)} className='login_form'>
                             <div className='input_container'>
                                 <input
