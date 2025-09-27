d11ff2a1f4d6961e7c86ab209d7f7f3a41ffde3d
VeckovN <novaknolewow@gmail.com>
2025-09-27 15:54:47 +0200

    fix: add missing ClientLayout.js from layout refactor

--- client/src/components/Layout/ClientLayout.js
+++ client/src/components/Layout/ClientLayout.js
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const ClientLayout = () =>{
-
     return(
         <>
             <Header></Header>
             <Outlet/>
+            <Footer></Footer>
         </>
     )
 }
