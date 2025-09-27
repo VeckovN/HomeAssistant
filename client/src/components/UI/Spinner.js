0bdb49e29b4e62269337a278c708f3a598e561ce
VeckovN <novaknolewow@gmail.com>
2025-09-27 16:44:42 +0200

    Add loading spinner to Login form and improve Spinner component

--- client/src/components/UI/Spinner.js
+++ client/src/components/UI/Spinner.js
@@ -1,17 +1,14 @@
-// import ClockLoader from 'react-spinners/ClockLoader'
-// import HashLoader from 'react-spinners/HashLoader'
 import PuffLoader from 'react-spinners/PuffLoader'
 
 const override = {
     display: "block",
     margin : "0 auto",
 }
-const Spinner = ({className}) =>{
+const Spinner = ({className, color = 'rgba(120, 240, 250, 1)'}) =>{
     return (
         <div className={className}>
             <PuffLoader
-                // color="rgba(180, 240, 250, 1)"
-                color="rgba(120, 240, 250, 1)"
+                color={color}
                 cssOverride={override}
                 speedMultiplier={2}
             /> 
