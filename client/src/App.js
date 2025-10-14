0357d95d643713ea0e863d11202b4ff7d4a73f34
VeckovN <novaknolewow@gmail.com>
2025-10-14 19:16:23 +0200

    feature: add RouteSuspense component with RouteLoadingFallback to replace Loading Screen as pre-route loader. Refactor ClientLayout and Footer to support layout and UI impovements

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
-import { Suspense, lazy } from 'react';
+import { lazy } from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { requestInterceptor } from './utils/AxiosConfig.js';
 import useSocket from './hooks/useSocket';
-import LoadingScreen from './components/UI/LoadingScreen.js';
 
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
@@ -13,6 +12,10 @@ import PrivateRoute from './utils/Route/PrivateRoute.js';
 import NotAuthRoute from './utils/Route/NotAuthRoute.js';
 import ScrollToTopWrapper from './utils/ScrollToTopWrapper.js';
 import NotificationWrapper from './utils/NotificationWrapper.js';
+import RouteSuspense from './utils/Route/RouteSuspense.js';
+
+import Sidebar from './components/Layout/Sidebar.js';
+import ClientLayout from './components/Layout/ClientLayout.js';
 
 const Register = lazy(() => import('./pages/Register/Register.js'));
 const ClientRegister = lazy(() => import('./pages/Register/Client/ClientRegister.js'));
@@ -26,8 +29,6 @@ const Messages = lazy(() => import('./pages/Messages.js'));
 const NotFound = lazy(() => import('./pages/NotFound.js'));
 const HouseworkerHome = lazy(() => import('./pages/Home/HouseworkerHome.js'));
 const ClientHome = lazy(() => import('./pages/Home/ClientHome.js'));
-const Sidebar = lazy(() => import('./components/Layout/Sidebar.js'));
-const ClientLayout = lazy(() => import('./components/Layout/ClientLayout.js'));
 
 function App() {
   const {user} = useSelector((state) => state.auth) 
@@ -36,93 +37,117 @@ function App() {
   requestInterceptor(dispatch);
 
   return (
-  <BrowserRouter>
-    <NotificationWrapper>
-      <ScrollToTopWrapper>
-        <Suspense fallback={<LoadingScreen/>}>
+    <BrowserRouter>
+      <NotificationWrapper>
+        <ScrollToTopWrapper>
           <Routes>
-
-          {(user?.type === "Client" || user == null) && (
-            <Route element={<ClientLayout/>}>
-              <Route index path="/" element={
+            {(user?.type === "Client" || user == null) && (
+              <Route 
+                 element={
+                  <ClientLayout />
+              }>
+                <Route index path="/" element={
                   <ClientHome socket={socket} />
-              }/>
-              <Route path="/login" element={
-                <NotAuthRoute>
-                    <Login/>
-                </NotAuthRoute>
-              }/>
-              <Route path="/register" element={
-                <NotAuthRoute>
-                    <Register/>
-                </NotAuthRoute>
-              }/>
-              <Route path="/clientRegister" element={
-                <NotAuthRoute>
-                    <ClientRegister />
-                </NotAuthRoute>
-              } />
-              <Route path="/HouseworkerRegister" element={
-                <NotAuthRoute>
-                    <HouseworkerRegister/>
-                </NotAuthRoute>
-              }/>
-              {/* <Route path="/profile" element={<PrivateRoute><Profile/></PrivateRoute>} /> */}
-              <Route path="/profile" element={
-                <PrivateRoute>
-                    <ClientProfile/>
-                </PrivateRoute>
-              }/>
-              <Route path="/messages" element={
-                <PrivateRoute>
-                    <Messages socket={socket} connected={connected}/>
-                </PrivateRoute>
-              }/>
-              <Route path="*" element={<NotFound/>} />
-            </Route>
-          )}
-
-          {user?.type === "Houseworker" && (
-            <Route element={<Sidebar/>} >
-              <Route index path="/" element={
-                <PrivateRoute>
-                    <HouseworkerHome/>
-                </PrivateRoute>
-              }/>
-              <Route path="/messages" element={
-                <PrivateRoute>
-                    <Messages socket={socket} connected={connected}/>
-                </PrivateRoute>
-              }/>
-              <Route path="/comments" element={
-                <PrivateRoute privacy='houseworker'>
-                    <Comments socket={socket} user={user}/>
-                </PrivateRoute>
-              }/>
-              {/* <Route path="/profile" element={<PrivateRoute><Profile/></PrivateRoute>} /> */}
-              <Route path="/profile" element={
-                <PrivateRoute>
-                    <HouseworkerProfile/>
-                </PrivateRoute>
-              }/>
-              <Route path="/login" element={
-                <NotAuthRoute>
-                    <Login/>
-                </NotAuthRoute>
-              }/>
-              <Route path="*" element={<NotFound/>} />
-            </Route>
-          )}
+                }/>
+                <Route path="/login" element={
+                  <RouteSuspense client={true}>
+                    <NotAuthRoute>
+                      <Login/>
+                    </NotAuthRoute>
+                  </RouteSuspense>
+                }/>
+                <Route path="/register" element={
+                  <RouteSuspense client={true}>
+                    <NotAuthRoute>
+                      <Register/>
+                    </NotAuthRoute>
+                  </RouteSuspense>
+                }/>
+                <Route path="/clientRegister" element={
+                  <RouteSuspense client={true}>
+                    <NotAuthRoute>
+                      <ClientRegister />
+                    </NotAuthRoute>
+                  </RouteSuspense>
+                } />
+                <Route path="/HouseworkerRegister" element={
+                  <RouteSuspense client={true}>
+                    <NotAuthRoute>
+                      <HouseworkerRegister/>
+                    </NotAuthRoute>
+                  </RouteSuspense>
+                }/>
+                <Route path="/profile" element={
+                  <RouteSuspense client={true}>
+                    <PrivateRoute>
+                      <ClientProfile/>
+                    </PrivateRoute>
+                  </RouteSuspense>
+                }/>
+                <Route path="/messages" element={
+                  <RouteSuspense client={true}>
+                    <PrivateRoute>
+                      <Messages socket={socket} connected={connected}/>
+                    </PrivateRoute>
+                  </RouteSuspense>
+                }/>
+                <Route path="*" element={
+                    <NotFound/>
+                } />
+              </Route>
+            )}
+             
+            {user?.type === "Houseworker" && (
+              <Route element={<Sidebar/>}>
+                <Route index path="/" element={
+                  <RouteSuspense>
+                    <PrivateRoute>
+                      <HouseworkerHome/>
+                    </PrivateRoute>
+                  </RouteSuspense>
+                }/>
+                <Route path="/messages" element={
+                  <RouteSuspense>
+                    <PrivateRoute>
+                      <Messages socket={socket} connected={connected}/>
+                    </PrivateRoute>
+                  </RouteSuspense>
+                }/>
+                <Route path="/comments" element={
+                  <RouteSuspense>
+                    <PrivateRoute privacy='houseworker'>
+                      <Comments socket={socket} user={user}/>
+                    </PrivateRoute>
+                  </RouteSuspense>
+                }/>
+                <Route path="/profile" element={
+                  <RouteSuspense>
+                    <PrivateRoute>
+                      <HouseworkerProfile/>
+                    </PrivateRoute>
+                  </RouteSuspense>
+                }/>
+                <Route path="/login" element={
+                  <RouteSuspense>
+                    <NotAuthRoute>
+                      <Login/>
+                    </NotAuthRoute>
+                  </RouteSuspense>
+                }/>
+                <Route path="*" element={
+                  <RouteSuspense>
+                    <NotFound/>
+                  </RouteSuspense>
+                } />
+              </Route>
+            )}
           </Routes>
-        </Suspense>
-  
-      <ToastContainer/>
-      </ScrollToTopWrapper>
-    </NotificationWrapper>
 
-  </BrowserRouter>
+          <ToastContainer/>
+        </ScrollToTopWrapper>
+      </NotificationWrapper>
+    </BrowserRouter>
   )
-
 }
 
 export default App;
---
+++ client/src/utils/Route/RouteSuspense.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { Suspense } from 'react';
+import HashLoader from 'react-spinners/HashLoader'
+
+import '../../sass/components/_routeLoadingFallback.scss'
+
+const RouteLoadingFallback = ({client}) => {
+    console.log("client: ", client);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setIsVisible(true), 100);
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <div className={`route-loading-fallback ${client ? 'client': ''}`}>
+            {isVisible && (
+            <div className="loading-screen-content">
+                <HashLoader 
+                    color="rgba(255, 255, 255, 0.9)"
+                    size={36}
+                    speedMultiplier={1.5}
+                />
+                <span className="route-loading-text">Loading...</span>
+            </div>
+            )}
+        </div>
+    );
+};
+
+const RouteSuspense = ({ children, client = false}) => {
+    return (
+        <Suspense fallback={<RouteLoadingFallback client={client}/>}>
+            {children}
+        </Suspense>
+    );
+};
+
+export default RouteSuspense;
