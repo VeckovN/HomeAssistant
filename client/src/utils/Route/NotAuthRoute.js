5c2e808f739da9cade4f73da2f91bd9250a92900
VeckovN <novaknolewow@gmail.com>
2025-10-19 20:58:53 +0200

    fix: NotAuthRoute authentication check to use Redux state instead of cookie

--- client/src/utils/Route/NotAuthRoute.js
+++ client/src/utils/Route/NotAuthRoute.js
@@ -1,14 +1,11 @@
 import { useSelector} from 'react-redux';
 import { Navigate} from 'react-router-dom'; 
-import Cookie from 'js-cookie';
 
 const NotAuthRoute = ({children}) =>{
     const {user} = useSelector((state) => state.auth)  
-    const userCookie = Cookie.get("user");
     const isAuthenticated = () =>{
-        if(userCookie){
+        if(user)
             return true;
-        }
         return false;
     }
 
